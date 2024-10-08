7081ea73fd026cdab844d39a81600e12f2d4fdc5
sjd-1214 <f230620@cfd.nu.edu.pk>
2024-10-08 13:02:12 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.listen(3000,(req,res)=>{
+    console.log('Server is started at Port 3000')
+    
+})
