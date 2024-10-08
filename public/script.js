75a5b0a5ad7db129e15c809210da630f6ed32046
sjd-1214 <f230620@cfd.nu.edu.pk>
2024-10-08 14:49:17 -0700

    branch commit

---
+++ public/script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    let converter = new showdown.Converter();
+    let pad = document.getElementById('pad');
+    let markdownArea = document.getElementById('markdown');
+
+    pad.addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
+
+
+    let convertTextAreaToMarkdown = function(){
+        var markdownText = pad.value;
+        html = converter.makeHtml(markdownText);
+        markdownArea.innerHTML = html;
+    };
+
+    pad.addEventListener('input', convertTextAreaToMarkdown);
+};
